e38f2645eba10e75897b93967d24de20114798db
Klemen Tušar <techouse@gmail.com>
2019-09-18 11:04:03 +0200

    :bug: Fix a number comparison bug that occurred when using displayUsingLabels

--- resources/js/components/Index/TextField.vue
+++ resources/js/components/Index/TextField.vue
@@ -10,7 +10,11 @@ export default {
     computed: {
         selectedValue() {
             if (typeof this.field["displayUsingLabels"] !== "undefined" && this.field["displayUsingLabels"] === true) {
-                return String(this.field.options.find(el => el.value === this.field.value).label)
+                const option = this.field.options.find(el => Number(el.value) === Number(this.field.value))
+                if (option) {
+                    return String(option.label)
+                }
+                return ""
             }
 
             return String(this.field.value)
