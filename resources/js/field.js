af7c96a9840195ac9e6d7f5e6a8f285e645eafec
Klemen Tušar <techouse@gmail.com>
2019-09-19 22:09:28 +0200

    :alien: Ditched Laravel Mix for plain Webpack + Babel

--- resources/js/field.js
+++ resources/js/field.js
@@ -1,5 +1,9 @@
-Nova.booting((Vue, router) => {
-    Vue.component("form-select-auto-complete", require("./components/Form/SelectField").default)
-    Vue.component("detail-select-auto-complete", require("./components/Detail/TextField").default)
-    Vue.component("index-select-auto-complete", require("./components/Index/TextField").default)
+import Index from "./components/Index"
+import Detail from "./components/Detail"
+import Form from "./components/Form"
+
+Nova.booting((Vue) => {
+    Vue.component("index-select-auto-complete", Index)
+    Vue.component("detail-select-auto-complete", Detail)
+    Vue.component("form-select-auto-complete", Form)
 })
